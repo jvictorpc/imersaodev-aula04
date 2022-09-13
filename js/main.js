9f2f5b2730019a00ff550564883448fac37c0617
victor <jvictorpc@gmail.com>
2022-09-13 16:36:13 -0300

    add input, e lógica para adicionar filmes a lista

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-const listaFilmes = ["https://m.media-amazon.com/images/I/91RlYXu-W0L._AC_SY550_.jpg","https://m.media-amazon.com/images/M/MV5BMTUxMzEzMzE5OV5BMl5BanBnXkFtZTgwNDk4NjI2MDE@._V1_.jpg", "https://m.media-amazon.com/images/M/MV5BNzViNmQ5MTYtMmI4Yy00N2Y2LTg4NWUtYWU3MThkMTVjNjk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg" ];
+const listaFilmes = ["https://m.media-amazon.com/images/I/91RlYXu-W0L._AC_SY550_.jpg", "https://m.media-amazon.com/images/M/MV5BMTUxMzEzMzE5OV5BMl5BanBnXkFtZTgwNDk4NjI2MDE@._V1_.jpg", "https://m.media-amazon.com/images/M/MV5BNzViNmQ5MTYtMmI4Yy00N2Y2LTg4NWUtYWU3MThkMTVjNjk3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"];
+
+const addFilme = document.querySelector('.add-filme');
+const secaoFilmes = document.querySelector('.filmes');
+
+const adicionaFilmes = (event) => {
+    event.preventDefault();
+    const posterFilme = document.querySelector('.input-filme').value;
+
+    const filme = document.createElement('img');
+    const src = document.createAttribute('src');
+
+    src.value = posterFilme;
+    filme.setAttributeNode(src);
+
+    secaoFilmes.appendChild(filme);  
+
+}
+
+addFilme.addEventListener('click', adicionaFilmes);
 
 
-for (let i = 0; i < listaFilmes.length; i++){
+
+/**
+ * for (let i = 0; i < listaFilmes.length; i++){
     document.write(`<img src="${listaFilmes[i]}"/>`);
 }
+
+secaoFilmes.innerHTML = `<img src="${posterFilme}"/>`;
+
+ */
